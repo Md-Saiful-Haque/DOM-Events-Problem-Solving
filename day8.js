0d93549882712c9e6ed4eec9b9cd95a79b9a66d6
Md Saiful Haque <saifulhaque.pciu@gmail.com>
2026-06-16 16:24:17 +0600

    Retry a Promise

--- day8.js
+++ day8.js
@@ -12,7 +12,24 @@ console.log(safeJsonParse('{"a":1}'));;
 
 // Problem 2: Retry a Promise
 
+async function retry(fn, times) {
+  let lastError;
+  
+  for (let i = 0; i < times; i++) {
+    try {
+      return await fn();
+    } catch (error) {
+      lastError = error;
+    }
+  }
+  
+  throw lastError;
+}
+
+
 // Problem 3: Implement Promise.all from Scratch
+
+
 
 // Problem 4: Flatten Object (Deep)
 
